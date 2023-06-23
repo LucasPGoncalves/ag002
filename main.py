f19f44cfc904412e4eed94d00168a67a5ac3ef89
Willyprp <89864941+Willyprp@users.noreply.github.com>
2023-06-23 15:45:04 -0300

    Ajuster

--- main.py
+++ main.py
@@ -35,29 +35,29 @@ tree.fit(X_train, y_train)
 tree_predict = tree.predict(X_test)
 tree_score = accuracy_score(y_test, tree_predict)
 
-# Printando a precisao do modelo (Metricas de Avaliacao)
-print(f"Precisao do modelo: {tree_score}")
+# Printando a precisão do modelo (Métricas de Avaliação)
+print(f"Precisão do modelo: {tree_score}")
 
-# Predicao
+# Predição
 while True:
     if input("0 para sair, qualquer outra tecla para continuar: ") == '0':
         break
 
-    idade = int(input("Entre com a faixa etaria | 1 - 9 : "))
-    menopausa = int(input("Entre com a situacao da menopausa | 1 - 3 : "))
-    tamTumor = int(input("Entre com a faixa do diametro do tumor | 1 - 12 : "))
-    linfonodos = int(input("Entre com o numero de linfonodos axilares | 1 - 13 : "))
-    penCaps = int(input("Entre com a penetracao do tumor na capsula do linfonodo | 1 - 2 : "))
+    idade = int(input("Entre com a faixa etária | 1 - 9 : "))
+    menopausa = int(input("Entre com a situação da menopausa | 1 - 3 : "))
+    tamTumor = int(input("Entre com a faixa do diâmetro do tumor | 1 - 12 : "))
+    linfonodos = int(input("Entre com o número de linfonodos axilares | 1 - 13 : "))
+    penCaps = int(input("Entre com a penetração do tumor na cápsula do linfonodo | 1 - 2 : "))
     grauMalig = int(input("Entre com o grau de malignidade do tumor | 1 - 3 : "))
-    mama = int(input("Entre com a mama que o cancer pode ocorrer | 1 - 2 : "))
+    mama = int(input("Entre com a mama que o câncer pode ocorrer | 1 - 2 : "))
     quadMama = int(input("Entre com o quadrante da mama afetado | 1 - 5 : "))
-    radioterapia = int(input("Entre com o historico de radioterapia | 1 - 2 : "))
+    radioterapia = int(input("Entre com o histórico de radioterapia | 1 - 2 : "))
 
     dados = np.array([idade, menopausa, tamTumor, linfonodos, penCaps, grauMalig, mama, quadMama, radioterapia]).reshape(-1,9)
 
     predicao = tree.predict(dados)
 
     if predicao == 2:
-        print("Ha possibilidade da recorrencia do cancer de mama")
+        print("Há possibilidade da recorrência do câncer de mama")
     elif predicao == 1:
-        print("Nao ha possibilidade da recorrencia do cancer de mama")
+        print("Não há possibilidade da recorrência do câncer de mama")
